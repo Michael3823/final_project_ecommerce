011cd1f63b7e6990361cdf290aecfdd8b9d0b050
Michael Merene <jmerene83@academic.rrc.ca>
2021-11-26 18:47:21 -0600

    added search for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,14 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.all.paginate(page: params[:page], per_page: 10)
   end
 
   def show
     @product = Product.find(params[:id])
     
+  end
+  def search
+    wildcard_search = "%#{params[:keywords]}%"
+    @products = Product.where("name LIKE ?", wildcard_search).paginate(page: params[:page], per_page: 10)
   end
 end

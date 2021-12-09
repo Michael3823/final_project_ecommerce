1d6f997649ba5a5028038a531cd25cc88b0c142c
Michael Merene <jmerene83@academic.rrc.ca>
2021-12-09 13:50:27 -0600

    make it better

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,6 +1,6 @@
 class CheckoutController < ApplicationController
   def create
-    product = Product.find(params[:id])
+     product = Product.find(params[:id])
     @session = Stripe::Checkout::Session.create({
       payment_method_types: ['card'],
       line_items: [{
@@ -10,14 +10,15 @@ class CheckoutController < ApplicationController
         quantity: 1
       }],
       mode: 'payment',
-      success_url: root_url,
+      success_url: checkout_success_url + "?session_id={CHECKOUT_SESSION_ID}",
       cancel_url: root_url,
     })
     redirect_to @session.url.to_s
   end
     
       def success
-        # We took the customer's money!
+        @session = Stripe::Checkout::Session.retrieve(params[:session_id])
+        @payment_intent = Stripe::PaymentIntent.retrieve(@session.payment_intent)
       end
     
       def cancel
@@ -25,3 +26,12 @@ class CheckoutController < ApplicationController
       end
     
 end
+# @session = Stripe::Checkout::Session.create({
+#   payment_method_types: ['card'],
+#   line_items: @cart.collect { |item| item.to_builder.attributes! },
+#   mode: 'payment',
+#   success_url: checkout_success_url + "?session_id={CHECKOUT_SESSION_ID}",
+#   cancel_url: root_url,
+# })
+# redirect_to @session.url.to_s
+# end
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,11 @@ class Product < ApplicationRecord
     validates :name,:description,:price, :quantity_available, presence:true
     validates :price, :quantity_available, numericality:true
 
+    def to_builder
+    Jbuilder.new do |product|
+      product.price stripe_price_id
+      product.quantity_available 1
+    end
+  end
+
 end
